30cc3adb342740ba13da5e3ebc0eb80e4af0295c
Grace Lingo <grace.lingo@bulsu.edu.ph>
2026-05-21 22:11:02 +0800

    hosting updated

--- config/hosting.php
+++ config/hosting.php
@@ -2,11 +2,11 @@
 // Copy this file to hosting.php and fill in your InfinityFree credentials.
 // Never commit hosting.php to source control.
 
-define('DB_HOST', 'sql106.infinityfree.com');
+define('DB_HOST', '127.0.0.1');
 define('DB_PORT', 3306);
-define('DB_USER', 'if0_40321620');
-define('DB_PASS', 'MMFSLnatoo');
-define('DB_NAME', 'if0_40321620_sampleweb');
+define('DB_USER', 'u957255050_marine_reg');
+define('DB_PASS', 'M~rphsx7!+/5');
+define('DB_NAME', 'u957255050_db_marine_reg');
 
 // After creating hosting.php with the above, upload it to config/ on the server.
 
